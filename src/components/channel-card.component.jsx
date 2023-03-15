1e3e25c57cfaff0d15d0e9435d0d0b585339e000
justasti <justas.tirevicius@gmail.com>
2023-03-15 19:57:09 +0200

    feat: channel page implemented

--- src/components/channel-card.component.jsx
+++ src/components/channel-card.component.jsx
@@ -3,9 +3,9 @@ import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
-    <Box sx={{ boxShadow: 'none', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: {sm: '356px', md: '320px'}, height: '326px', margin: 'auto' }}>
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: {sm: '356px', md: '320px'}, height: '326px', margin: 'auto', marginTop }}>
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent
           sx={{
@@ -38,8 +38,7 @@ const ChannelCard = ({ channelDetail }) => {
             <Typography>
               {parseInt(
                 channelDetail?.statistics?.subscriberCount
-              ).toLocaleString()}
-              Subscribers
+              ).toLocaleString()} Subscribers
             </Typography>
           )}
         </CardContent>
