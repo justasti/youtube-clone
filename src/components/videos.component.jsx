a9b7c3931a1c158a5c522180ba603891647e8b31
justasti <justas.tirevicius@gmail.com>
2023-03-15 19:34:13 +0200

    feat: channel card implemented

--- src/components/videos.component.jsx
+++ src/components/videos.component.jsx
@@ -2,11 +2,12 @@ import { Stack, Box } from '@mui/material'
 import { VideoCard, ChannelCard } from '../components'
 
 const Videos = ({ videos }) => {
+  console.log(videos)
   return (
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
       {videos.map((video, idx) => (
         <Box key={idx}>
-          {video.id.videoId && <VideoCard video={video} />}
+          {(video.id.videoId || video.id.playlistId) && <VideoCard video={video} />}
           {video.id.channelId && <ChannelCard channelDetail={video} />}
         </Box>
       ))}
